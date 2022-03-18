d23ad2c377d415869f1dc7f0b14b219761b9e9d5
unPoulpe <yayafeu2@gmail.com>
2022-03-18 12:35:33 +0100

    TD 18/03/22

--- AP2/exo1.c
+++ AP2/exo1.c
@@ -3,15 +3,29 @@
 int main()
 {
 
-    int x;
-    int *gigapointeur;
+    // int x;
+    // int *gigapointeur;
 
-    gigapointeur = &x;
+    // gigapointeur = &x;
 
-    printf("Entrer un entier:\n");
-    scanf("%i", &x);
-    printf("la variable x :\n%i", *gigapointeur);
-    printf("\nl'addresse de x :\n%p", gigapointeur);
+    // printf("Entrer un entier:\n");
+    // scanf("%i", &x);
+    // printf("la variable x :\n%i", *gigapointeur);
+    // printf("\nl'addresse de x :\n%p", gigapointeur);
+
+    int tab[3];
+    int i;
+
+    for (i = 0; i < 3; i++)
+    {
+        printf("Entrer l'entier d'indice %i : ", i);
+        scanf("%i", &tab[i]);
+    }
+
+    for (i = 0; i < 3; i++)
+    {
+        printf("L'entier d'indice %i est %i : son addresse est %p \n", i, tab[i], &tab[i]);
+    }
 
     return 0;
 }
